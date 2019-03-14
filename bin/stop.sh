28d353684c18ec8d9a729998c2badb55287fbc21
nico <ainililia@163.com>
2019-03-14 18:43:21 +0800

    更新脚本

--- bin/stop.sh
+++ bin/stop.sh
@@ -1,4 +1,4 @@
-pids=$(ps -ef |grep 'java -jar yasso.jar'| awk '{print $2}') 
+pids=$(ps -ef |grep 'java -jar'|grep 'yasso.jar'| awk '{print $2}') 
 for pid in $pids; do
     kill -9 $pid
     echo $pid killed
